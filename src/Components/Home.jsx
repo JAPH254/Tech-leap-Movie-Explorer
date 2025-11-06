5fa8b7a4f52cca5c92e6b30fef9da0624f36f5a7
JAPH254 <omanyujapheth@gmail.com>
2025-11-06 09:47:08 +0300

    refactor: update Home component styling for improved layout

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
 
   return (
     <>
-      <div className="App">
+      <div className="flex flex-col items-center justify-center min-h-screen py-2 px-4 bg-gray-100">
         <h1>Movie Explorer</h1>
         <SearchMovies />
         <TrendingMovies />
