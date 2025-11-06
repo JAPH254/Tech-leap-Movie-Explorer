213610521d8f108480c2c023169e069baf79652a
JAPH254 <omanyujapheth@gmail.com>
2025-11-06 09:33:04 +0300

    feat: add route for Register component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 import Home from "./Components/Home";
 import ProtectedRoute from "./Components/ProtectedRoute";
 
@@ -7,6 +8,7 @@ export default function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
       <Route
         path="/home"
         element={
